99dfe1bf21743c0cc90c7bbffcb92815d6213948
Jung Sae Yan <saeyan99@naver.com>
2025-05-03 18:30:20 +0900

    modified 폰켓몬.py

--- programmers/algorithm/해시/폰켓몬.py
+++ programmers/algorithm/해시/폰켓몬.py
@@ -2,8 +2,7 @@ def solution(nums):
     answer = 0
     
     cnt = len(nums)//2 #선택하는 포켓몬 수
-    pon = set(nums) # 종류 
-    n_pon = len(pon) # 종류 개수 
+    n_pon = len(set(nums)) # 종류 개수 
     answer = min(cnt, n_pon) # 둘 중 더 작은 값 반환
     
     return answer
